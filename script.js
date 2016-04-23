c0453012c2fd6d515ada2aa7ebcb8ad037a7cca1
Katarina Makmuri <katarina.makmuri@gmail.com>
2016-04-23 13:00:19 -0700

    not complete

---
+++ script.js
@@ -0,0 +1,14 @@
+<script type="text/javascript" src="jquery-1.8.3.js"></script>
+$('h1').text('Welcome to this web page,' + yourName);
+$('h1').css('background-color', 'green');
+$('p').css('color', 'orange');
+$('p').text('We\'re \nproud \nof our basic skillzz')
+
+
+var yourName = prompt("What's your name?");
+
+
+
+
+
+var yourAge = prompt("How old are you?");
